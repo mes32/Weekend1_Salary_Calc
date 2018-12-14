f7bb73f77d851b2e26109c103866a35b05e095f1
mes32 <stockman.mike@gmail.com>
2018-12-14 16:15:25 -0600

    Add some basic CSS styling.

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,6 +1,14 @@
 
-
 $(onReady);
 function onReady() {
     console.log('JQ in onReady()');
+    addEventHandlers();
+}
+
+function addEventHandlers() {
+    $('#add-employee-button').on('click', addEmployee);
+}
+
+function addEmployee() {
+    console.log('in addEmployee()');
 }
